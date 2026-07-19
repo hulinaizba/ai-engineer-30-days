99d85762817b58461041a84f50bfac4c388afef2
hulinaizba <ezhenkov090@gmail.com>
2026-07-19 23:33:34 +0300

    Add day01 and hello_ai.py

---
+++ day01/hello_ai.py
@@ -0,0 +1,25 @@
+import datetime
+
+name = input("Какое твое имя? ")
+
+while True:
+
+    age_input = input("Сколько тебе лет? ")
+    if age_input.isdigit():
+        age = int(age_input)
+        break
+    else:
+        print("Пожалуйста, введите число.")
+
+year_now = datetime.date.today().year
+age_in_2030 = age + (2030 - year_now)
+
+today = datetime.date.today()
+end_date = today + datetime.timedelta(days=30)
+days_left = (end_date - today).days
+
+print("_" * 40)
+print(f"Привет, {name}!")
+print(f"В 2030 году тебе будет {age_in_2030} лет.")
+print(f"До конца курса осталось {days_left} дней.") 
+print("_" * 40)
