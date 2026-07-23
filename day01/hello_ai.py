716828ee1cbebda899ff3ef01ce80ceea32e55d5
hulinaizba <ezhenkov090@gmail.com>
2026-07-23 11:26:31 +0300

    Fix: hello_ai days_left now counts correctly

--- day01/hello_ai.py
+++ day01/hello_ai.py
@@ -15,7 +15,7 @@ year_now = datetime.date.today().year
 age_in_2030 = age + (2030 - year_now)
 
 today = datetime.date.today()
-end_date = today + datetime.date(2026, 8, 17)
+end_date = datetime.date(2026, 8, 17)
 days_left = (end_date - today).days
 
 print("_" * 40)
